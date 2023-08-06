7fa13398a84ea77f728910382d5e3ddf1fed2af6
Urmila-Chaudhary <urmilachaudhary2407@gmail.com>
2023-08-06 18:10:20 +0530

    occur

---
+++ occur.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+ int a[20],n; 
+void insert() {
+ int i;
+ printf("Enter size of array : ");
+ scanf("%d",&n);
+ printf("Enter elements\n");
+ for(i=0;i<n;i++) {
+ scanf("%d",&a[i]);
+ }
+   }
+ void occur() {
+ int num,i,occ[20],occpos=-1;
+ printf("Enter the element to be searched:\n");
+ scanf("%d",&num);
+ for(i=0;i<n;i++) {
+ if(a[i]==num) {
+ occpos++;
+ occ[occpos] = i;
+ }
+ }
+ if(occpos==-1) {
+ printf("Element not found");
+ }
+ else {
+ printf("Element found at positions : ");
+ for(i=0;i<=occpos;i++) {
+ printf("%d\t",occ[i]);
+  }
+ }
+   }
+void main() 
+{
+ insert();
+ occur();
+ } 
