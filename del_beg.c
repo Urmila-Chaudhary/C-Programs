9d50531f2f2ccdcbb35df2574432b9f764e0a914
Urmila-Chaudhary <urmilachaudhary2407@gmail.com>
2023-09-28 18:10:20 +0530

    del_beg

---
+++ del_beg.c
@@ -0,0 +1,73 @@
+#include<stdio.h> 
+#include<malloc.h> 
+struct node{
+ int data;
+ struct node *next;
+ };
+ struct node *head=NULL; 
+void insert(int e) {
+ struct node *t;
+ if(head == NULL) {
+ head = (struct node *)malloc(sizeof(struct node));
+ head -> data = e;
+ head -> next = NULL;
+ }
+ else {
+ t=head;
+ while(t->next!=NULL)  {
+ t=t->next;
+ }
+ t->next=(struct node *)malloc(sizeof(struct node));
+ t->next->data=e;
+ t->next->next=NULL;
+ }
+  }
+ void delete_from_start() {
+ if (head==NULL) printf("Linked list is empty!!\n\n");
+ else
+ {
+ head = head->next; 
+}
+ } 
+void disp() {
+ struct node *t;
+ if (head==NULL) 
+  printf("Linked list is empty!!\n\n");
+ else {
+ t=head;
+ printf("\n");
+ while(t!=NULL) {
+ printf("%d\t",t->data);
+ t=t->next;
+ }
+ printf("\n");
+ }
+  } 
+int menu() {
+ int ch;
+ printf("Insert – 1, Delete from beginning – 2, Display – 3, EXIT - 4\n");
+ printf("Enter your choice : ");
+ scanf("%d",&ch);
+ return ch;
+ }
+ int main() {
+ int ch;
+ for(ch = menu();ch != 4;ch = menu()) {
+ switch(ch) {
+ case 1 : printf("Enter the element to insert : ");
+ scanf("%d",&ch);
+ insert(ch);
+ break;
+ case 2 : delete_from_start();
+ break;
+ case 3 : disp();
+ break;
+ default : printf("Wrong Choice!!!\n");
+ break;
+ }; 
+printf("\n");
+ } 
+return 0; 
+} 
+ 
+ 
