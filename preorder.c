17fb39ced23b29f2f9c903c611403a2ad32dfcb0
Urmila-Chaudhary <urmilachaudhary2407@gmail.com>
2023-10-14 18:10:20 +0530

    preorder

---
+++ preorder.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+typedef struct node tree;
+
+tree *root = NULL;
+tree *stack[20];
+int top = -1;
+
+void insert(int e) {
+    tree *p, *x, *t;
+
+    if (root == NULL) {
+        root = (tree *)malloc(sizeof(tree));
+        root->data = e;
+        root->left = NULL;
+        root->right = NULL;
+    } else {
+        p = root;
+        while (p != NULL) {
+            x = p;
+            if (p->data > e) {
+                p = p->left;
+            } else if (p->data < e) {
+                p = p->right;
+            } else {
+                printf("Repeated value not allowed!\n");
+                return;
+            }
+        }
+        
+        t = (tree *)malloc(sizeof(tree));
+        t->data = e;
+        t->left = NULL;
+        t->right = NULL;
+        
+        if (x->data < e) {
+            x->right = t;
+        } else {
+            x->left = t;
+        }
+    }
+}
+
+void preorder(tree *r) {
+    if (r != NULL) {
+        printf("%d\t", r->data);
+        preorder(r->left);
+        preorder(r->right);
+    }
+}
+
+void push(tree *e) {
+    if (top == 19) {
+        printf("Stack is full\n");
+    } else {
+        stack[++top] = e;
+    }
+}
+
+tree *pop() {
+    if (top == -1) {
+        return NULL;
+    } else {
+        return stack[top--];
+    }
+}
+
+void preorder2(tree *root) {
+    tree *t = root;
+    tree *temp;
+    
+    while (1) {
+        if (t != NULL) {
+            push(t);
+            printf("%d\t", t->data);
+            t = t->left;
+        } else {
+            if (top == -1) {
+                break;
+            }
+            temp = pop();
+            t = temp->right;
+        }
+    }
+}
+
+int main() {
+    insert(50);
+    insert(15);
+    insert(70);
+    insert(10);
+    insert(20);
+    insert(55);
+    insert(90);
+    insert(13);
+    insert(60);
+    
+    printf("\nPreorder without using recursion:\n");
+    preorder2(root);
+    printf("\n");
+    
+    return 0;
+}
