4936f2040031c1e65c10537e60f6275e31eed662
Urmila-Chaudhary <161577372+Urmila-Chaudhary@users.noreply.github.com>
2024-05-30 15:48:24 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+int checkPrime(int num)
+{
+ int i,flag=1;
+ if(num==1)
+ return 0;
+ for(i=2;i<=sqrt(num);i++){
+     if(num%i==0){
+         flag=0;break;
+     }
+ }
+ return flag;
+}
+
+
+void main()
+{   
+    int num;
+    int result;
+    scanf("%d",&num);
+    result=checkPrime(num);
+    printf("%d",result);
+}
+
+
